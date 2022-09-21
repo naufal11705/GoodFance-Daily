a3801e8a8d556ca258d2f8fec7c1491383438cda
KrizaFauzi <92284062+KrizaFauzi@users.noreply.github.com>
2022-09-21 12:05:36 +0700

    first commit

---
+++ resources/views/homepage/kontak.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.template')
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col">
+      <h1>Selamat anda kena prank!!</h1>
+    </div>
+  </div>
+</div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HomePageController;
+use App\Http\Controllers\DashboardController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [\App\Http\Controllers\HomePageController::class,'index']);
+Route::get('/about', [\App\Http\Controllers\HomePageController::class,'about']);
+Route::get('/kontak', [\App\Http\Controllers\HomePageController::class,'kontak']);
+Route::get('/kategori', [\App\Http\Controllers\HomePageController::class,'kategori']);
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('/', [\App\Http\Controllers\DashboardController::class,'index']);
+    
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
