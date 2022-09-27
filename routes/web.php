3f9871d9b7fe282dfa15f47c4b7e9aa5a8d6a7fd
AthenaAsamiya <ahmadnaufal11705@gmail.com>
2022-09-27 20:13:38 +0700

    Merge branch 'main' of https://github.com/KrizaFauzi/GoodFance

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,8 @@ use App\Http\Controllers\HomePageController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TransaksiController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\LaporanController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,7 +30,12 @@ Route::get('/category', [\App\Http\Controllers\HomePageController::class,'katego
 
 Route::group(['prefix' => 'admin'], function() {
     Route::get('/', [\App\Http\Controllers\DashboardController::class,'index']);
-    
+
+    Route::get('profil', [\App\Http\Controllers\UserController::class,'index']);
+    Route::get('setting', [\App\Http\Controllers\UserController::class,'setting']);
+
+    Route::get('laporan', [\App\Http\Controllers\LaporanController::class,'index']);
+    Route::get('proseslaporan', [\App\Http\Controllers\LaporanController::class,'proses']);
 });
 
 Route::resource('kategori', \App\Http\Controllers\KategoriController::class);
